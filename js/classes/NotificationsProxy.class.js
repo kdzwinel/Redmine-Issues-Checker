ef79b83cc80fe6004f96dc11982734ae3baea493
Narno <arnaud@ligny.org>
2014-08-26 12:23:01 +0200

    Deleting the extended notitifcation feature
    webKitNotifications has been deprecated

--- js/classes/NotificationsProxy.class.js
+++ js/classes/NotificationsProxy.class.js
@@ -1,35 +1,5 @@
 var NotificationsProxy = (function () {
 
-    function formatTime(time) {
-        var createdOn = new Date(time);
-        var today = new Date();
-
-        if(createdOn.getDate() == today.getDate() && createdOn.getMonth() == today.getMonth() && createdOn.getYear() == today.getYear()) {
-            var timeNow = today.getHours() * 60 + today.getMinutes();
-            var timeIssue = createdOn.getHours() * 60 + createdOn.getMinutes();
-
-            if(timeNow - timeIssue == 0) {
-                return 'today (just now)';
-            } else if((timeNow - timeIssue) < 60 && (timeNow - timeIssue) > 0) {
-                return 'today (' + (timeNow-timeIssue) +' minute(s) ago)';
-            } else {
-                var hours = createdOn.getHours();
-                if(hours < 10) {
-                    hours = '0'+hours;
-                }
-
-                var minutes = createdOn.getMinutes();
-                if(minutes < 10) {
-                    minutes = '0'+minutes;
-                }
-
-                return 'today (' + hours + ':' + minutes + ')';
-            }
-        } else {
-            return createdOn.getDate() + '.' + (createdOn.getMonth()+1) + '.' + createdOn.getFullYear();
-        }
-    }
-
     function createNotification(settings, issue) {
         var opt = {
             type: "basic",
@@ -37,48 +7,6 @@ var NotificationsProxy = (function () {
             message: issue.description,
             iconUrl: "/img/redmine_logo_128.png"
         };
-
-        if (settings.get('notificationsType') == 'extended') {
-            var items = [];
-            if (settings.get('notificationFieldAuthor')) {
-                items.append({
-                    title: 'Author',
-                    message: issue.author.name
-                });
-            }
-            if (settings.get('notificationFieldTime')) {
-                items.append({
-                    title: 'Created',
-                    message: formatTime(issue.created_on)
-                });
-            }
-            if (settings.get('notificationFieldProject')) {
-                items.append({
-                    title: 'Project Name',
-                    message: issue.project.name
-                });
-            }
-            if (settings.get('notificationFieldPriority')) {
-                items.append({
-                    title: 'Priority',
-                    message: issue.priority.name
-                });
-            }
-            if (settings.get('notificationFieldTracker')) {
-                items.append({
-                    title: 'Tracker',
-                    message: issue.tracker.name
-                });
-            }
-
-            opt = {
-                type: "list",
-                title: '"' + issue.subject + '" by ' + issue.author.name,
-                message: "Primary message to display",
-                iconUrl: "/img/redmine_logo_128.png",
-                items: items
-            }
-        }
 
         chrome.notifications.create(Math.random().toString(), opt, function (nid) {
             console.log('Notification ID: ' + nid);
@@ -88,38 +16,9 @@ var NotificationsProxy = (function () {
         });
     }
 
-    function createWebNotification(settings, issue) {
-        var notification;
-
-        if (settings.get('notificationsType') == 'extended' && webkitNotifications.createHTMLNotification) {
-            notification = webkitNotifications.createHTMLNotification(
-                '/notification.html'
-            );
-
-            chrome.extension.getBackgroundPage().setNewIssue(issue);
-        } else {
-            notification = webkitNotifications.createNotification(
-                '/img/redmine_logo_128.png',  // icon url - can be relative
-                '"' + issue.subject + '" by ' + issue.author.name,  // notification title
-                issue.description  // notification body text
-            );
-        }
-
-        notification.show();
-
-        window.setTimeout(function () {
-            notification.cancel();
-        }, settings.get('notificationsTimeout'));
-    }
-
     function create(settings, issue) {
-        if (webkitNotifications && webkitNotifications.createHTMLNotification) {
-            //use web notifications API
-            createWebNotification(settings, issue);
-        } else if (chrome.notifications) {
-            //use new Notifications API (https://developer.chrome.com/extensions/notifications.html)
-            createNotification(settings, issue);
-        }
+        //use new Notifications API (https://developer.chrome.com/extensions/notifications.html)
+        createNotification(settings, issue);
     }
 
     return {
--- js/storeInit.js
+++ js/storeInit.js
@@ -11,21 +11,10 @@ var settings = new Store("settings", {
 	'updateDelayMinMS': 60000,
 
 	'showNotifications': false,
-	'notificationsTimeout': 5000,
-	'notificationsType': 'standard',
-
-	'notificationFieldAuthor': true,
-	'notificationFieldTime': true,
-	'notificationFieldProject': true,
-	'notificationFieldPriority': false,
-	'notificationFieldTracker': false,
-
-	'notificationBgColor': 'white',
-	'notificationTextColor': 'black',
-	'notificationLinkColor': '#961b25'
+	'notificationsTimeout': 5000
 });
 
-var oldOptions = ['redmineUrl', 'showIssues', 'userLogin', 'userPassword', 'iconPressUrl', 'iconPressDontRedirect', 'showNotifications', 'notificationsTimeout', 'notificationsType', 'notificationFieldAuthor', 'notificationFieldPriority', 'notificationFieldProject', 'notificationFieldTime', 'notificationFieldTracker', 'notificationLinkColor', 'notificationTextColor', 'notificationBgColor'];
+var oldOptions = ['redmineUrl', 'showIssues', 'userLogin', 'userPassword', 'iconPressUrl', 'iconPressDontRedirect', 'showNotifications', 'notificationsTimeout'];
 
 //import old options
 for(var idx in oldOptions) {
@@ -36,7 +25,7 @@ for(var idx in oldOptions) {
 
 		if(optionName == 'notificationsTimeout') {
 			oldValue = oldValue * 1000;
-		} else if(optionName == 'iconPressDontRedirect' || optionName == 'showNotifications' || optionName == 'notificationFieldAuthor' || optionName == 'notificationFieldPriority' || optionName == 'notificationFieldProject' || optionName == 'notificationFieldTime' || optionName == 'notificationFieldTracker') {
+		} else if(optionName == 'iconPressDontRedirect' || optionName == 'showNotifications') {
 			oldValue = (oldValue == '1');
 		}
 
